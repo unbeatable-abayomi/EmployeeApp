36ec820625f81b3bb237222d3a19d4ffe51db192
abayomi <53374703+unbeatable-abayomi@users.noreply.github.com>
2020-04-17 15:39:25 +0100

    Add project files.

---
+++ EmployeeApp/EmpArrayList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+
+namespace EmployeeApp
+{
+    class EmpArrayList
+    {
+        private int EmpID;
+        private string Name;
+        private int Age;
+        ArrayList myOwnArryList = new ArrayList();
+
+
+
+
+        public EmpArrayList( string nam, int empId, int ag)
+        {
+            EmpID = empId;
+            Name = nam;
+            Age = ag;
+            
+            myOwnArryList.Add(Name);
+            myOwnArryList.Add(EmpID);
+            myOwnArryList.Add(Age);
+        }
+
+
+
+
+
+        public void GetEmployee( int identity)
+        {
+            int validateId = identity;
+           // myOwnArryList.Contains(validateId);
+            Console.WriteLine(myOwnArryList.Contains(validateId));
+            if (myOwnArryList.Contains(validateId))
+            {
+                Console.Write("Name:");
+                for(int i = 0; i < myOwnArryList.Count; i++)
+                {
+                    Console.Write($"{myOwnArryList[i]} : ");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Sorry We can't Validate you; Your ID is in Valid");
+            }
+           
+
+            
+                
+
+        }
+    }
+}
---
+++ EmployeeApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            EmpArrayList empArrayList1 = new EmpArrayList("abayomi",12673,24);
+            EmpArrayList empArrayList2 = new EmpArrayList("ada", 23904, 14);
+            EmpArrayList empArrayList3 = new EmpArrayList("kemi",56899, 34);
+            EmpArrayList empArrayList4 = new EmpArrayList("Kunle",66471, 54);
+            EmpArrayList empArrayList5 = new EmpArrayList("Seun", 125431, 74);
+            empArrayList1.GetEmployee(234);
+           
+
+
+        }
+    }
+}
